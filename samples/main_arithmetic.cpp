89b3103397d6a75f6d359a80c8328c4c8c78909d
PolinaKapustina <76258216+PolinaKapustina@users.noreply.github.com>
2022-02-27 00:18:41 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,26 @@
 // реализация пользовательского приложения
 
+
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "stack.h"
+#include "arithmetic.h"
+
+using namespace std;
+
 int main()
 {
-  return 0;
+	cout << "Enter your expression, no spaces." << endl;
+	cout << "Allowed symbols: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ., /, *, -, +" << endl;
+	double res;
+	string s;
+	cin >> s;
+	PolishNotation v(s);
+	v.TranslationToPolishNotation();
+	cout << "Polish notation:" << endl;
+	v.PrintPolishNotation();
+	res = v.PolishNotationCalculate();
+	cout << endl << "Result:" << endl << res << endl;
 }
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,3 +2,56 @@
 
 #include "stack.h"
 #include <gtest.h>
+#include "stack.h"
+#include <gtest.h>
+
+TEST(TStack, can_create_stack_with_positive_length)
+{
+	ASSERT_NO_THROW( TStack<int> st(5));
+}
+
+TEST(TStack, cant_create_stack_with_wrong_length)
+{
+	ASSERT_ANY_THROW( TStack<int> st(0), st1(-1));
+}
+
+TEST(TStack, cant_create_stack_with_too_big_length)
+{
+	ASSERT_ANY_THROW( TStack<int> st(MaxStackSize + 1));
+}
+
+
+TEST(TStack, can_get_size_of_stack)
+{
+	TStack<int> st(5);
+	ASSERT_NO_THROW(st.Get());
+}
+
+TEST(TStack, can_push_elem_in_stack)
+{
+	TStack<int> st(5);
+	ASSERT_NO_THROW(st.Push(5));
+}
+
+TEST(TStack, cant_push_101th_elem_in_stack)
+{
+	TStack<int> st(30);
+	for (int i = 0; i < 100; i++)
+		st.Push(i);
+	ASSERT_ANY_THROW(st.Push(101));
+}
+
+TEST(TStack, can_get_top_elem_in_stack)
+{
+	TStack<int> st(5);
+	st.Push(5);
+	EXPECT_EQ(5, st.GetHeadElement());
+}
+
+TEST(TStack, can_pop_elem_in_stack)
+{
+	TStack<int> st(5);
+	st.Push(5);
+	st.Pop();
+	EXPECT_EQ(0, st.Get());
+}
