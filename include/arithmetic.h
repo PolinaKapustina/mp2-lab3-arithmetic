5953ce4e1798b114d13ed4ec9f7e859342373389
PolinaKapustina <76258216+PolinaKapustina@users.noreply.github.com>
2022-02-27 00:17:07 +0300

    Add files via upload

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,40 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#pragma once
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "stack.h"
+
+using namespace std;
+struct Lexem
+{
+	int priority; // приоритетность 
+	string type; // тип (число\операци€)
+	string lexm; // лексема
+
+	Lexem& operator = (const Lexem& other)
+	{
+		this->priority = other.priority;
+		this->lexm = other.lexm;
+		this->type = other.type;
+
+		return *this;
+	}
+};
+
+class PolishNotation
+{
+private:
+	string formula; // выражение
+	vector <Lexem> polish;//польска€ (постфиксна€) запись
+	vector <Lexem> l;
+public:
+	PolishNotation(string s); // конструктор
+	void CorrectionChecker(string s); // проверка корректности
+	double BinaryCalculate(double a, double b, string s); // математические операции
+	void TranslationToPolishNotation(); // польскка€ запись
+	void PrintPolishNotation(); // вывод польской записи
+	double PolishNotationCalculate();
+};
+
--- include/stack.h
+++ include/stack.h
@@ -1,9 +1,95 @@
 // объявление и реализация шаблонного стека
 // стек поддерживает операции: 
-// - вставка элемента, 
-// - извлечение элемента, 
-// - просмотр верхнего элемента (без удаления)
-// - проверка на пустоту, 
-// - получение количества элементов в стеке
-// - очистка стека
-// при вставке в полный стек должна перевыделяться память
+// - вставка элемента, +
+// - извлечение элемента, +
+// - просмотр верхнего элемента (без удаления) +
+// - проверка на пустоту, +
+// - получение количества элементов в стеке +
+// - очистка стека +
+// при вставке в полный стек должна перевыделяться память +
+int MaxStackSize = 100;
+
+template <class T>
+class TStack
+{
+private:
+	T* stack;
+	int size;
+	int head;
+
+public:
+
+	TStack(int _size)
+	{
+		if ((_size < 1) || (_size > MaxStackSize)) throw ("razmer");
+		head = -1;
+		size = _size;
+		stack = new T[size];
+	}
+	~TStack()
+	{
+		delete[] stack;
+	}
+
+	bool TStack<T>::IsEmpty() // проверка на пустоту
+	{
+		if (head == -1)
+			return true;
+		else
+			return false;
+	}
+
+	bool TStack<T>::IsFull() // - проверка на полноту
+	{
+		if (head == (size - 1))
+			return true;
+		else
+			return false;
+	}
+
+	T TStack<T>::Pop() // извлечение элемента
+	{
+		if (IsEmpty() == 1)
+			throw "Stack is Empty";
+		else
+			return stack[head--];
+	}
+	T TStack<T>::GetHeadElement() // Просмотр верхнего элемента без удалаения
+	{
+		if (IsEmpty()==1)
+		{
+			throw "Stack is Empty";
+		}
+		return steak[head];
+	}
+
+	int TStack<T>::Get() // получение количества элементов в стеке
+	{
+		return head++;
+	}
+
+	void Push(T elem) // вставка элемента
+	{
+		if (head + 1 == size)
+		{
+			size = size * 2;
+			T* temp = new T[size];
+			for (int i = 0; i < size/2; i++)
+				temp[i] = stack[i];
+			delete[] stack;
+			stack = temp;
+		}
+		stack[head + 1] = elem;
+		head++;
+	}
+
+	void Clear() // очистка стека
+	{
+		head = -1;
+		delete[] stack;
+		stack = new T[size];
+	}
+
+};
+
+
