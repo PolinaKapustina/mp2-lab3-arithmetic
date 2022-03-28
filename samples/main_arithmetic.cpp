c36cbd091f1d6a054cfbdf0d5af9023cf1384a7f
PolinaKapustina <76258216+PolinaKapustina@users.noreply.github.com>
2022-03-28 22:36:30 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -39,6 +39,27 @@ int main()
 			v.PrintPolishNotation();
 			res = v.PolishNotationCalculate();
 			cout << endl << "Result:" << endl << res << endl;
+			cout << "Would you like to calculate a new expression? Yes or no?" << endl;
+			string str;
+			int k = 0;
+			while (k == 0)
+			{
+				cin >> str;
+				if (str == "yes" || str == "Yes" || str == "YES")
+				{
+					cout << "Enter your expression, no spaces." << endl;
+					cout << "Allowed symbols: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, x, y, z, ., /, *, -, +" << endl;
+					chet = 0;
+					k = -1;
+				}
+				else
+				{
+					if (str == "NO" || str == "No" || str == "no")
+						k = -1;
+					else
+						cout << "It seems you made a mistake in your answer. Enter the answer again, it must be 'yes' or 'no'." << endl;
+				}
+			}
 		}
 	}
 }
